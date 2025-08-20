2ccc2e3fe463fbfe8ca59750564da298f6a18e2f
Pablo S <cineschonffeldt@gmail.com>
2025-08-20 15:46:00 -0500

    Allow files from external server config

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,3 +1,15 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <main>Home page</main>;
+  return (
+    <main className="bg-[#5dc9a8] min-h-screen">
+      <Image
+        src="https://bytegrad.com/course-assets/react-nextjs/petsoft-preview.png"
+        alt="Preview of PetSoft"
+        width={519}
+        height={472}
+      />
+      <div></div>
+    </main>
+  );
 }
