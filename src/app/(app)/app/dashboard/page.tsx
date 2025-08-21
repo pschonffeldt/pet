ad2ce77ad8e09e13daadfdfee6e575980cd9d089
Pablo S <cineschonffeldt@gmail.com>
2025-08-21 12:35:05 -0500

    Create new components and add to dashboard

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,3 +1,14 @@
+import Branding from "@/components/branding";
+import Stats from "@/components/stats";
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <main>
+      <div className="flex items-center justify-between text-white py-8">
+        <Branding />
+
+        <Stats />
+      </div>
+    </main>
+  );
 }
