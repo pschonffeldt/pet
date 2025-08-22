24a9bddb2306f7a9a016c61ad2c5b7b7e9c0ddbd
Pablo S <cineschonffeldt@gmail.com>
2025-08-22 12:05:23 -0500

    Create search context provider file and control search form to filter the pet names

---
+++ src/context/search-context-provider.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { createContext, useState } from "react";
+
+type SearchContextProvider = {
+  children: React.ReactNode;
+};
+
+type TSearchContext = {
+  searchQuery: string;
+  handleChangeSearchQuery: (newValue: string) => void;
+};
+
+export const SearchContext = createContext<TSearchContext | null>(null);
+
+export default function SearchContextProvider({
+  children,
+}: SearchContextProvider) {
+  // state
+  const [searchQuery, setsearchQuery] = useState("");
+  // derived state
+
+  // event handlers / actions
+  const handleChangeSearchQuery = (newValue: string) => {
+    setsearchQuery(newValue);
+  };
+
+  return (
+    <SearchContext.Provider
+      value={{
+        searchQuery,
+        handleChangeSearchQuery,
+      }}
+    >
+      {children}
+    </SearchContext.Provider>
+  );
+}
