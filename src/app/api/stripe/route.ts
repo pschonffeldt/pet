c1186aff41ef219bfa892e7c1fe98a5fa4c01083
Pablo S <cineschonffeldt@gmail.com>
2025-09-05 12:36:41 -0500

    Makse sure incoming webhook is actually from stripe

--- src/app/api/stripe/route.ts
+++ src/app/api/stripe/route.ts
@@ -1,20 +1,40 @@
 import prisma from "@/lib/db";
 
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 export async function POST(request: Request) {
-  const data = await request.json();
+  const body = await request.text();
+  const signature = request.headers.get("stripe-signature");
 
-  //   verify webhook came from stripe
+  // verify webhook came from Stripe
+  let event;
+  try {
+    event = stripe.webhook.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_WEBHOOK_SECRET
+    );
+  } catch (error) {
+    console.log("Webhook verification failed", error);
+    return Response.json(null, { status: 400 });
+  }
 
-  //   fulfill order
-  await prisma.user.update({
-    where: {
-      email: data.data.object.customer_email,
-    },
-    data: {
-      hasAccess: true,
-    },
-  });
+  // fulfill order
+  switch (event.type) {
+    case "checkout.session.completed":
+      await prisma.user.update({
+        where: {
+          email: event.data.object.customer_email,
+        },
+        data: {
+          hasAccess: true,
+        },
+      });
+      break;
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
 
-  //   return 200 ok
+  // return 200 OK
   return Response.json(null, { status: 200 });
 }
