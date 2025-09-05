597eef88b19e38bc1e66961342563011edbec9cf
Pablo S <cineschonffeldt@gmail.com>
2025-09-05 08:54:11 -0500

    Change redirect rules

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -50,14 +50,19 @@ const config = {
         return false;
       }
 
+      if (isLoggedIn && isTryingToAccessApp && !auth?.user.hasAccess) {
+        return Response.redirect(new URL("/payment", request.nextUrl));
+      }
+
       if (isLoggedIn && isTryingToAccessApp && auth?.user.hasAccess) {
         return true;
       }
-      // redirect to dashboard is faulty!
+
       if (isLoggedIn && !isTryingToAccessApp) {
         if (
-          request.nextUrl.pathname.includes("/login") ||
-          request.nextUrl.pathname.includes("/signup")
+          (request.nextUrl.pathname.includes("/login") ||
+            request.nextUrl.pathname.includes("/signup")) &&
+          !auth?.user.hasAccess
         ) {
           return Response.redirect(new URL("/payment", request.nextUrl));
         }
